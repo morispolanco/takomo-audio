373bd166ac10fa9d65efa4f54f3f7840893bf9c8
morispolanco <morispolanco@gmail.com>
2023-12-19 17:29:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import requests
+from pydub import AudioSegment
+import tempfile
+import os
 
-def transcribe_and_improve_voice(api_key, audio_url):
+def transcribe_and_improve_voice(api_key, audio_data):
     api_url = "https://api.takomo.ai/62391ec5-cb73-40b0-afa5-9279a7f1060c"
 
     headers = {
@@ -9,11 +12,15 @@ def transcribe_and_improve_voice(api_key, audio_url):
         "Authorization": f"Bearer tk_258c8e8ff4d803a9adad65032fc283d72ccf953cb3910caedba786d420b36e39a2b3e309d529da25c3c2fa3625485947"
     }
 
-    payload = {
-        "audio_url": audio_url
-    }
+    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_audio:
+        temp_audio_path = temp_audio.name
+        audio_data.export(temp_audio_path, format="wav")
 
-    response = requests.post(api_url, json=payload, headers=headers)
+    with open(temp_audio_path, "rb") as audio_file:
+        files = {"audio_file": audio_file}
+        response = requests.post(api_url, files=files, headers=headers)
+
+    os.remove(temp_audio_path)
 
     if response.status_code == 200:
         result = response.json()
@@ -24,15 +31,18 @@ def transcribe_and_improve_voice(api_key, audio_url):
 def main():
     st.title("Transcripción y Mejora de Notas de Voz con Streamlit")
 
-    audio_url = st.text_input("Introduce la URL del archivo de audio:")
+    audio_file = st.file_uploader("Cargar archivo de audio (mp3, wav, m4a)", type=["mp3", "wav", "m4a"])
     api_key = st.text_input("Introduce tu API Key:")
 
     if st.button("Transcribir y Mejorar"):
-        if not audio_url or not api_key:
-            st.warning("Por favor, introduce la URL del archivo de audio y la API Key.")
+        if not audio_file or not api_key:
+            st.warning("Por favor, carga un archivo de audio y introduce la API Key.")
         else:
             st.info("Transcribiendo y mejorando... Esto puede tomar un tiempo.")
-            result = transcribe_and_improve_voice(api_key, audio_url)
+            
+            audio_data = AudioSegment.from_file(audio_file, format=audio_file.name.split('.')[-1])
+            result = transcribe_and_improve_voice(api_key, audio_data)
+            
             st.success(f"Transcripción mejorada:\n\n{result}")
 
 if __name__ == "__main__":
