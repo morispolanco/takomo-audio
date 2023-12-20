46b2e9f96d58b51cdd83a3cfa38583ad8bd097be
morispolanco <morispolanco@gmail.com>
2023-12-20 03:04:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,37 +3,35 @@ import time
 import streamlit as st
 
 API_KEY = "tk_258c8e8ff4d803a9adad65032fc283d72ccf953cb3910caedba786d420b36e39a2b3e309d529da25c3c2fa3625485947"
-ENDPOINT_ID = "62391ec5-cb73-40b0-afa5-9279a7f1060c" 
+ENDPOINT_ID = "62391ec5-cb73-40b0-afa5-9279a7f1060c"
 
 st.title("Transcriptor de audio")
 
-# Transcribe function
+audio_file = st.file_uploader("Sube un archivo de audio")
+
+@st.cache
 def transcribe(audio):
 
-  files = {"audio": audio}
+  # Transcription request
+  
+if audio_file is not None:
 
-  headers = {"Authorization": f"Bearer {API_KEY}"}
+  try:
 
-  response = requests.post(f"https://api.takomo.ai/{ENDPOINT_ID}/transcription",
-                           files=files,
-                           headers=headers)
+    job_response = transcribe(audio_file)
+    job_id = job_response.json()['id']
 
-  return response
+    # Check status loop 
 
-# Rest of code...
+    result = status_response.json()['data']
+    transcript = result['transcript']
 
-if audio_file:
+  except Exception as e:
+    st.write(f"Error: {e}")
 
-  job_response = transcribe(audio_file)
+  else:
+    st.write(f"Transcripción: {transcript}")
 
-  # Get job ID  
-  job_id = job_response.json()['id']
+else:
 
-  # Check status loop
-  while status != 'completed':
-
-    status_response = requests.get(f"https://api.takomo.ai/{ENDPOINT_ID}/transcription/{job_id}")
-
-    # Process response
-
-  # Return transcript
+  st.write("Por favor selecciona un archivo de audio")
