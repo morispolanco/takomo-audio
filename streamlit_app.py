6e4a5584d00f99e8d505139111b4950acf8bc7eb
morispolanco <morispolanco@gmail.com>
2023-12-20 02:43:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,40 @@
 import streamlit as st
 import requests
+import io
 
 API_KEY = "tk_258c8e8ff4d803a9adad65032fc283d72ccf953cb3910caedba786d420b36e39a2b3e309d529da25c3c2fa3625485947"
 
-st.title("Transcriptor y amplificador de notas de voz")
+st.title("Transcriptor de audio")
 
 audio_file = st.file_uploader("Sube un archivo de audio")
 
-if audio_file is not None:
+if audio_file:
 
-  files = {"audio": audio_file}
+  try:
 
-  headers = {"Authorization": f"Bearer {API_KEY}"}
+    audio = io.BytesIO(audio_file.read())
 
-  response = requests.post("https://api.takomo.ai/62391ec5-cb73-40b0-afa5-9279a7f1060c", 
-                            files=files,
-                            headers=headers)
+    files = {"audio": audio}
 
-  data = response.json()
+    headers = {"Authorization": f"Bearer {API_KEY}"}
 
-  st.write("Transcripción:")
-  st.write(data["transcript"])
+    response = requests.post("https://api.takomo.ai/endpoint",
+                             files=files,
+                             headers=headers)
 
-  st.write("Amplificación:")
-  st.write(data["expanded_transcript"])
+  except Exception as e:
+    st.write(f"Error en la petición: {e}")
+
+  else:
+
+    data = response.json()
+
+    st.write("Respuesta de la API:")
+    st.write(data)
+
+    try:
+      transcript = data["transcript"]
+    except KeyError:
+      st.write("Respuesta no contiene campo transcript")
+    else:  
+      st.write(f"Transcripción: {transcript}")
