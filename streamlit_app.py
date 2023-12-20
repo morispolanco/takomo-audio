667087d8280e222978372b633fdb49b7e3818a1c
morispolanco <morispolanco@gmail.com>
2023-12-20 02:59:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,41 @@
-import streamlit as st
 import requests
-import io
 import time
+import streamlit as st
 
 API_KEY = "tk_258c8e8ff4d803a9adad65032fc283d72ccf953cb3910caedba786d420b36e39a2b3e309d529da25c3c2fa3625485947"
-ENDPOINT = "https://api.endpoint"
+BASE_URL = "https://api.takomo.ai/"
 
-st.title("Transcriptor")
+st.title("Transcriptor de audio")
 
-audio = st.file_uploader("Sube audio")
+audio_file = st.file_uploader("Sube un archivo de audio")
 
-if audio:
-
-  files = {"audio": io.BytesIO(audio.read())}
+@st.cache
+def transcribe(audio):
+  files = {"audio": audio}
   headers = {"Authorization": f"Bearer {API_KEY}"}
 
+  response = requests.post(BASE_URL + "transcription", 
+                           files=files, 
+                           headers=headers)
+  return response
+
+if audio_file:
   try:
-    response = requests.post(ENDPOINT, files=files, headers=headers)
-    response.raise_for_status()
-    response_data = response.json()
+    job_response = transcribe(audio_file)
+    job_id = job_response.json()['id']
 
-  except Exception as e:
-    st.write(f"Error en la petición: {e}")
-
-  else:
-    
-    job_id = response_data["id"]
-
-    while True:
-
-      status = requests.get(f"{ENDPOINT}/{job_id}").json()
-
-      if status["status"] == "completed":
-        break
-
-      st.write("Procesando...")
+    status = None
+    while status != 'completed':
+      status_response = requests.get(BASE_URL + f"transcription/{job_id}")  
+      status = status_response.json()['status']
+      st.write(f"Procesando... Status: {status}")
       time.sleep(5)
 
-    try:  
-      transcript = status["data"]["transcript"]
-    except KeyError:
-      st.write("Error en transcripción")
-    else:
-      st.write(f"Transcripción: {transcript}")
+    result = status_response.json()['data']
+    transcript = result['transcript']
+
+  except Exception as e:
+    st.write(f"Error: {e}")
+
+  else:
+    st.write(f"Transcripción: {transcript}")
