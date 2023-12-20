310b0f8cc1542386c0315af7560c695a199dd6da
morispolanco <morispolanco@gmail.com>
2023-12-20 02:50:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,41 @@
-
-
 import streamlit as st
 import requests
 import io
+import time
+
 API_KEY = "tk_258c8e8ff4d803a9adad65032fc283d72ccf953cb3910caedba786d420b36e39a2b3e309d529da25c3c2fa3625485947"
-ENDPOINT_URL = "https://api.takomo.ai/62391ec5-cb73-40b0-afa5-9279a7f1060c"
+ENDPOINT_URL = "https://api.takomo.ai/endpoint"
 
 st.title("Transcriptor de audio")
 
-audio_file = st.file_uploader("Sube un archivo de audio")
+audio_file = st.file_uploader("Sube un archivo")
 
 if audio_file:
 
+  audio = io.BytesIO(audio_file.read())
+
+  files = {"audio": audio}
+  headers = {"Authorization": f"Bearer {API_KEY}"}
+
+  response = requests.post(ENDPOINT_URL, files=files, headers=headers)
+
+  job_id = response["id"]
+
+  while True:
+
+    status_response = requests.get(f"{ENDPOINT_URL}/{job_id}")
+
+    if status_response["status"] == "completed":
+      break
+
+    st.write("Procesando...")
+    time.sleep(5)
+
+  data = status_response["data"]
+
   try:
-
-    audio = io.BytesIO(audio_file.read())
-
-    files = {"audio": audio}
-
-    headers = {"Authorization": f"Bearer {API_KEY}"}
-
-    response = requests.post(ENDPOINT_URL, 
-                             files=files,
-                             headers=headers)
-
-  except Exception as e:
-    st.write(f"Error en la petición: {e}")
-
+    transcript = data["transcript"]
+  except KeyError:
+    st.write("Error en la transcripción")
   else:
-
-    try:
-      data = response.json()
-    except Exception:
-      st.write("Error al procesar la respuesta")
-    else:
-
-      st.write("Respuesta de la API:")
-      st.write(data)
-
-      try:
-        transcript = data["transcript"]
-      except KeyError:
-        st.write("Respuesta no contiene campo transcript")  
-      else:
-        st.write(f"Transcripción: {transcript}")
+    st.write(f"Transcripción: {transcript}")
